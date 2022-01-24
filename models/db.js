c0c912811f00ac4409ed9e448769570a313c9eef
Tom <72255127+thomasgardner4PU@users.noreply.github.com>
2022-01-24 13:24:13 +0000

    App Version 1 - Created database connection
    database connection is now established

---
+++ models/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://tguop2021:inctVlOgj8JcW7BK@cluster0.riszx.mongodb.net/projectsDB?retryWrites=true&w=majority"
+
+// connect method of mongoose
+
+mongoose.connect(url, {useNewUrlParser:true}, (err) => {
+    if(!err) {
+        console.log("mongodb connection is successful");
+    }
+    else {
+        // if any error is there
+
+        console.log("An error occured when connecting to mongodb" + err);
+    }
+})
