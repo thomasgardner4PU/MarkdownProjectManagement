1d13a4558dd0518a7d39844892dc52c78666620b
Tom <72255127+thomasgardner4PU@users.noreply.github.com>
2022-01-24 13:03:17 +0000

    App Version 1 - Created server.js
    nodemon now knows to stay online even when changes are made.
    
    Express, body-parser and handlbars are now imported
    
    middleware is now setup
    
    json format conversion for request data is implemented
    
    director name for views folder is setup using path library
    
    view engine is now setup
    
    listening port is working as expected

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressHandlebars = require('express-handlebars');
+
+let app = express();
+
+// middleware
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use(bodyParser.json()); // it will eb converting all the request data to json format
+
+// configuring the views of application
+app.set('views', path.join(__dirname, '/views/'));
+
+app.engine('hbs', expressHandlebars.engine({
+    extname:'hbs',
+    defaultLayout:'mainLayout',
+    layoutDir: __dirname + '/views/'
+}));
+
+app.set('view engine', 'hbs'); // successfully configured the express handlebars
+
+// configuring the route for the home page
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on Port 5000");
+})
