6f5dce3a5badb8c6380d898fdeb80b52d9963553
Tom <72255127+thomasgardner4PU@users.noreply.github.com>
2022-01-24 13:54:09 +0000

    Update projectController.js
    we can now insert data into the document within mongodb , however we cannot get project/list to display all the projects

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+// include the model class
+const Project = mongoose.model('Project');
+
 const router = express.Router();
 
 // route of projects
@@ -8,5 +13,33 @@ router.get('/', (req, res) => {
         viewTitle: "Insert Project"
     })
 });
+
+// handle the post request
+router.post('/', (req, res) => {
+    // creating custom function
+
+    insertProject(req,res);
+});
+
+function insertProject(req,res) {
+
+    let project = new Project();
+
+    project.title = req.body.title;
+    project.email = req.body.email;
+    project.fullname = req.body.fullname;
+    project.description = req.body.description;
+
+    project.save((err, doc) => {
+        // if there is no error
+
+        if(!err){
+            res.redirect('project/list');
+        }
+        else{
+            console.log("An error was detected while inserting a record");
+        }
+    });
+}
 
 module.exports = router;
